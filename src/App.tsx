0cc82eb35d2c72614eabd291f5d3de23a3510fe9
Son Tran Thanh <286.trants@gmail.com>
2024-07-10 22:33:15 +0700

    feat(layout): add locales layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 // PROJECTS
 import ThemeCustomization from '~/themes';
 import RTLLayout from '~/components/RTLLayout';
+import Locales from '~/components/Locales';
 
 export default function App() {
   return (
     <ThemeCustomization>
       <RTLLayout>
-        <h1>This is App.tsx</h1>;
+        <Locales>
+          <h1>This is App.tsx</h1>;
+        </Locales>
       </RTLLayout>
     </ThemeCustomization>
   );
