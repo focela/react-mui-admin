f485873ace2f24e8765d23888f7a1542f590cc74
Son Tran Thanh <286.trants@gmail.com>
2024-07-10 22:36:21 +0700

    feat(layout): add scroll top layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@
 import ThemeCustomization from '~/themes';
 import RTLLayout from '~/components/RTLLayout';
 import Locales from '~/components/Locales';
+import ScrollTop from '~/components/ScrollTop';
 
 export default function App() {
   return (
     <ThemeCustomization>
       <RTLLayout>
         <Locales>
-          <h1>This is App.tsx</h1>;
+          <ScrollTop>
+            <h1>This is App.tsx</h1>;
+          </ScrollTop>
         </Locales>
       </RTLLayout>
     </ThemeCustomization>
