8e217c7535b06137fbd223d1376379168572f471
Son Tran Thanh <286.trants@gmail.com>
2024-07-10 16:59:17 +0700

    feat(layout): add RTL layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 // PROJECTS
 import ThemeCustomization from '~/themes';
+import RTLLayout from '~/components/RTLLayout';
 
 export default function App() {
   return (
     <ThemeCustomization>
-      <h1>This is App.tsx</h1>;
+      <RTLLayout>
+        <h1>This is App.tsx</h1>;
+      </RTLLayout>
     </ThemeCustomization>
   );
 }
