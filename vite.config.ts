a8cee7cb3150ea37e13d36f86c0242fab3101694
Son Tran Thanh <286.trants@gmail.com>
2024-07-10 10:26:08 +0700

    feat(refactor): restructure directory structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: process.env.VITE_APP_BASE_NAME,
+  plugins: [react(), viteTsconfigPaths()],
+  define: {
+    global: 'window'
+  },
+  resolve: {
+    alias: []
+  },
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000
+  }
 })
