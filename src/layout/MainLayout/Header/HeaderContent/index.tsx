328a5a4a9410f1180b76df9776a626e322455b41
Son Tran Thanh <286.trants@gmail.com>
2024-07-15 09:25:54 +0700

    feat(layout): add search section

--- src/layout/MainLayout/Header/HeaderContent/index.tsx
+++ src/layout/MainLayout/Header/HeaderContent/index.tsx
@@ -2,11 +2,17 @@ import useConfig from '~/hooks/useConfig';
 import { Theme, useMediaQuery } from '@mui/material';
 import { LAYOUT_CONST } from '~/types/config';
 import DrawerHeader from '~/layout/MainLayout/Drawer/DrawerHeader';
+import SearchSection from '~/layout/MainLayout/Header/HeaderContent/SearchSection';
 
 export default function HeaderContent() {
   const { layout } = useConfig();
 
   const matchDownLg = useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'));
 
-  return <>{layout === LAYOUT_CONST.HORIZONTAL && !matchDownLg && <DrawerHeader open={true} />}</>;
+  return (
+    <>
+      {layout === LAYOUT_CONST.HORIZONTAL && !matchDownLg && <DrawerHeader open={true} />}
+      {!matchDownLg && <SearchSection />}
+    </>
+  );
 }
